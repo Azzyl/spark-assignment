8b345d6dacd8e8313bad1b2bb6a6c15fd3d1778d
Azzyl <belg.azat@gmail.com>
2018-11-26 01:11:36 +0300

    + Library is normally used + Creating fat jar (sbt -> 0.13) + Getting RSS content from url

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,35 @@
+import com.github.catalystcode.fortis.spark.streaming.rss._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Hel {
-
+object RSSDemo {
   def main(args: Array[String]) {
-
-    //Create a SparkContext to initialize Spark
-    val conf = new SparkConf()
-    conf.setMaster("local")
-    conf.setAppName("Word Count")
+    val durationSeconds = 10
+    val conf = new SparkConf().setAppName("RSS Spark Application").setIfMissing("spark.master", "local[*]")
     val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(durationSeconds))
+    sc.setLogLevel("ERROR")
 
-    // Load the text into a Spark RDD, which is a distributed representation of each line of text
-    val textFile = sc.textFile("src/main/resources/shakespeare.txt")
+    val urlCSV = args(0)
+    val urls = urlCSV.split(",")
+    val stream = new RSSInputDStream(urls, Map[String, String](
+      "User-Agent" -> "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+    ), ssc, StorageLevel.MEMORY_ONLY, pollingPeriodInSeconds = durationSeconds)
+    stream.foreachRDD(rdd => {
+      val spark = SparkSession.builder().appName(sc.appName).getOrCreate()
+      import spark.sqlContext.implicits._
+      val tweets = rdd.toDS()
+      rdd.foreach(tweet => {
+        println(tweet.uri)
+        println(tweet.title)
+        println(tweet.content)
+      })
+    })
 
-    //word count
-    val counts = textFile.flatMap(line => line.split(" "))
-      .map(word => (word, 1))
-      .reduceByKey(_ + _)
-
-    counts.foreach(println)
-    System.out.println("Total words: " + counts.count());
-    counts.saveAsTextFile("/tmp/shakespeareWordCount")
+    // run forever
+    ssc.start()
+    ssc.awaitTermination()
   }
-
 }
