53adaa35521dfc27919c9267d57c50762dee9415
Azzyl <belg.azat@gmail.com>
2018-11-26 21:10:26 +0300

    + Training model on kaggle dataset + Predicting

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,14 @@
 import com.github.catalystcode.fortis.spark.streaming.rss._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.sql.{SparkSession, functions}
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
+
+
+case class TrainingTweet(id: Long, label: Integer, text: String)
 
 object RSSDemo {
   def main(args: Array[String]) {
@@ -12,24 +17,43 @@ object RSSDemo {
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(durationSeconds))
     sc.setLogLevel("ERROR")
-
     val urlCSV = args(0)
     val urls = urlCSV.split(",")
     val stream = new RSSInputDStream(urls, Map[String, String](
       "User-Agent" -> "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
     ), ssc, StorageLevel.MEMORY_ONLY, pollingPeriodInSeconds = durationSeconds)
+    val spark = SparkSession.builder().appName(sc.appName).getOrCreate()
+    import spark.sqlContext.implicits._
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val tweets = sqlContext.read.format("com.databricks.spark.csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("train.csv").as[TrainingTweet]
+      .withColumn("text", functions.lower(functions.col("text")))
+//      .select("ItemId", "SentimentText", "Sentiment")
+    val tokenizer = new Tokenizer()
+      .setInputCol("text")
+      .setOutputCol("words")
+    val hashingTF = new HashingTF()
+      .setNumFeatures(10000)
+      .setInputCol(tokenizer.getOutputCol)
+      .setOutputCol("features")
     val lr = new LogisticRegression()
-//    var model1 = lr.fit()
+      .setMaxIter(10)
+      .setRegParam(0.001)
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, hashingTF, lr))
+    tweets.printSchema()
+    val model = pipeline.fit(tweets)
     stream.foreachRDD(rdd => {
-      val spark = SparkSession.builder().appName(sc.appName).getOrCreate()
-      import spark.sqlContext.implicits._
+      val columnName = "title"
       val tweets = rdd.toDS()
-      rdd.foreach(tweet => {
-        println(tweet.uri)
-        println(tweet.title)
-        tweet.content.foreach(rssContent => println(rssContent.value))
-        println(tweet.content.length)
-      })
+        .select("uri", "title")
+        .withColumn(columnName, functions.lower(functions.col(columnName)))
+        .withColumn("id", functions.monotonically_increasing_id())
+        .withColumn("text", functions.col("title"))
+      val result = model.transform(tweets).select("uri", "probability", "prediction")
+      result.show()
     })
 
     // run forever
